4bd905e76a2e9bd12edad7e7b5c0efd2ca77e189
martriay <martriay@gmail.com>
2015-02-04 13:23:37 -0300

    retab cdll

--- doubly_linked_list/doubly_linked_list.go
+++ doubly_linked_list/doubly_linked_list.go
@@ -1,41 +1,41 @@
 package main
 
 type list struct {
-	first *list_node
-	last  *list_node
+  first *list_node
+  last  *list_node
 }
 
 type list_node struct {
-	prev *list_node
-	next *list_node
-	data interface{}
+  prev *list_node
+  next *list_node
+  data interface{}
 }
 
 func (self *list) Append(x interface{}) {
-	new_node := new(list_node)
-	new_node.data = x
+  new_node := new(list_node)
+  new_node.data = x
 
-	if (*self).first == nil {
-		self.first = new_node
-		self.last = new_node
-	}
+  if (*self).first == nil {
+    self.first = new_node
+    self.last = new_node
+  }
 
-	new_node.prev = self.last
-	new_node.next = self.first
-	self.last = new_node
+  new_node.prev = self.last
+  new_node.next = self.first
+  self.last = new_node
 
-	new_node.prev.next = new_node
-	new_node.next.prev = new_node
+  new_node.prev.next = new_node
+  new_node.next.prev = new_node
 }
 
 func (self *list) Len() (length int) {
-	if self.first != nil {
-		length++
-	}
+  if self.first != nil {
+    length++
+  }
 
-	for f := self.first; f != self.last; f = f.next {
-		length++
-	}
+  for f := self.first; f != self.last; f = f.next {
+    length++
+  }
 
-	return
+  return
 }
